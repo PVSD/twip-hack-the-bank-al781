a1cf5a05992368ca94a944432af5e5578072369b
Alexander <al781@pvsd.org>
2019-02-19 17:36:54 -0500

    asdfghjkl

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,32 +4,43 @@ import java.io.*;
 import java.util.*;
 import java.text.*;
 public class Main {
+    public static ArrayList<String> log = new ArrayList<String>();
+    public static ArrayList<bankAccount> aryLst = new ArrayList<bankAccount>();
+    public static bankAccount someGuysBankAccount = new bankAccount("Abraham Pennebacker",0);
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         NumberFormat fmt = NumberFormat.getNumberInstance();
         fmt.setMinimumFractionDigits(2);
         fmt.setMaximumFractionDigits(2);
         String name;
-        ArrayList aryLst = new ArrayList();
-        ListIterator iter = aryLst.listIterator();
+
         do {
             Scanner kbReader = new Scanner(System.in);
             System.out
                     .print("Please enter the name to whom the account belongs. (\"Exit\" to abort) ");
             name = kbReader.nextLine();
             if (!name.equalsIgnoreCase("EXIT")) {
+                if (name.equalsIgnoreCase("debug")) {
+                    DebugMode();
+                }
                 System.out.print("Please enter the amount of the deposit. ");
                 double amount = kbReader.nextDouble();
                 System.out.println(" "); // gives an eye pleasing blank line
                 // between accounts
                 bankAccount theAccount = new bankAccount(name, amount);
-                iter.add(theAccount);
+                aryLst.add(theAccount);
+                String s = "Created Bank Account for " + name + "containing" + amount;
+                log.add(s);
+                for (int i = 0; i < aryLst.size(); i++)
+                    for (int j = 0; j < aryLst.size(); j++)
+                        if (aryLst.get(j).balance > aryLst.get(j+1).balance)
+                            Collections.swap(aryLst,j,j+1);
             }
         } while (!name.equalsIgnoreCase("EXIT"));
 
         // Search aryLst and print out the name and amount of the largest bank
-        // account
+        ListIterator iter = aryLst.listIterator();
         bankAccount ba = (bankAccount) iter.previous();
         double maxBalance = ba.balance; // set last account as the winner so far
         String maxName = ba.name;
@@ -46,5 +57,51 @@ public class Main {
                 + maxName + ".");
         System.out.println("The amount is $" + fmt.format(maxBalance) + ".");
 
+    }
+
+
+    public static void DebugMode() {
+        Scanner numScan = new Scanner(System.in);
+        System.out.println("DEBUG MODE INITIATED\nPICK AN OPTION");
+        System.out.println("1) LIST ACCOUNTS");
+        System.out.println("2) DRAIN ACCOUNTS");
+        System.out.println("3) EXIT");
+        switch(numScan.nextInt())
+            {
+                case 1:
+                    ListAccounts();
+                    break;
+                case 2:
+                    DrainAccounts();
+                    break;
+                case 3:
+                    //Does nothing
+                    break;
+                default:
+                    System.out.println("ENTER A VALID CODE");
+                    DebugMode();
+                    break;
+            }
+    }
+    public static void ListAccounts()
+    {
+        for (int i = 0; i < aryLst.size(); i++)
+            {
+                System.out.println(aryLst.get(i).name + " : " + aryLst.get(i).balance);
+                DebugMode();
+            }
+    }
+    public static void DrainAccounts()
+    {
+        for (int i = 0; i > aryLst.size(); i++)
+            {
+                double a =  aryLst.get(i).balance;
+                aryLst.get(i).withdraw(a);
+                String s = "Withdraw of " + a + " from " + aryLst.get(i).name;
+                log.add(s);
+                someGuysBankAccount.deposit(a);
+                s = "Deposit of " + a + " to " + someGuysBankAccount.name;
+                log.add(s);
+            }
     }
 }
